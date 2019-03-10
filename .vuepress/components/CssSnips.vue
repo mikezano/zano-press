44755b39c560df7cb9d3f0e3990a095260bd8695
mikezano <mikezano@gmail.com>
2019-03-09 20:55:51 -0700

    omg...typed.js can't handle displayed semicolons

--- .vuepress/components/CssSnips.vue
+++ .vuepress/components/CssSnips.vue
@@ -1,10 +1,14 @@
 <template>
 	<div>
-		<div id="css-snippet-type-static" class="language-css extra-class">
+		<!-- <div id="css-snippet-type-static" class="language-css extra-class">
 			<pre id="typing-static" class="language-css"></pre>
 		</div>
 		<div id="css-snippet-type" class="language-css extra-class">
 			<pre id="typing" class="language-css"></pre>
+		</div>-->
+		<div id="css-snippets" class="language-css extra-class"></div>
+		<div id="typed-container" class="language-css extra-class">
+			<pre id="typed" class="language-css"></pre>
 		</div>
 	</div>
 </template>
@@ -17,46 +21,58 @@ const componentsSource = ctxraw.keys().map(ctxraw);
 
 import Prism from 'prismjs';
 import Typed from 'typed.js';
+import { variableDeclaration } from 'babel-types';
 
 export default {
 	data() {
 		return {
 			cssSnippets: [],
-			currentSnippet: "",
+			currentSnippet: '',
 			snippetsLength: 0,
 			interval: null,
 		};
 	},
-	methods:{
+	methods: {
 		getRandomInt(max) {
 			return Math.floor(Math.random() * max);
 		},
-		start(){
-			setInterval(()=>{
-				let nextSnippetIndex = this.getRandomInt(this.snippetsLength);
-				this.currentSnippet = this.cssSnippets[nextSnippetIndex];
-			}, 2000);
-		}
+		start() {
+			var cssSnippets = document.getElementById('css-snippets');
+			this.cssSnippets.forEach(snippet => {
+				var html = Prism.highlight(snippet, Prism.languages.css, 'css');
+				var preTag = document.createElement('pre');
+				preTag.classList.add('language-css');
+				preTag.innerHTML = html;
+				cssSnippets.appendChild(preTag);
+			});
+			// setInterval(() => {
+			// 	let nextSnippetIndex = this.getRandomInt(this.snippetsLength);
+			// 	this.currentSnippet = this.cssSnippets[nextSnippetIndex];
+			// 	//var prismEl = document.getElementById('allStrings');
+			// 	//prismEl.innerHTML =
+
+			// }, 2000);
+		},
+		beforeTyping(arrayPos, self) {},
 	},
 	created() {
-
-		this.cssSnippets = componentsSource[0].split("/**/");
+		this.cssSnippets = componentsSource[0].split('/**/');
 		this.snippetsLength = this.cssSnippets.length;
-
 	},
 	mounted() {
-
-		var options ={
+		var options = {
+			stringsElement: '#css-snippets',
 			strings: this.cssSnippets,
 			typeSpeed: 50,
 			backDelay: 1000,
-			backSpeed: 5000,
+			backSpeed: 30,
 			smartBackspace: false,
 			loop: true,
+			preStringTyped: this.beforeTyping,
 		};
-		
-		var elTyped = document.getElementById('typing');
-		var typed = new Typed('#typing', options);
+
+		//var elTyped = document.getElementById('typing');
+		//var typed = new Typed('#typing', options);
 
 		this.start();
 
@@ -66,34 +82,39 @@ export default {
 		// 	typingStatic.innerHTML = elTyped.innerHTML;
 		// }, 10);
 
+		var typed = new Typed('#typed', options);
+		//typed.options.backSpaceHtmlChars = this.myOwn;
 	},
-	beforeDestroy(){
+	beforeDestroy() {
 		clearInterval(this.interval);
-	}
-
+	},
 };
 </script>
 
 <style lang="scss">
-	#css-snippet-type-static{
-		position: absolute;
-		top:6rem;
-		font-size:2rem;
-		width:28rem;
-		height:20rem;
-		left:50%;
-		margin-left:-14rem;
-	}
-
-	#css-snippet{
-		display:none;
-	}
-	#css-snippet-type{
-		//display:none;
-	}
-	
-	#typing-static{
+#typed-container {
+	position: absolute;
+	top: 6rem;
+	font-size: 2rem;
+	width: 28rem;
+	height: 20rem;
+	left: 50%;
+	margin-left: -14rem;
+	white-space: pre;
+	pre {
 		white-space: pre;
-		font-family: 'Roboto Slab', serif;
 	}
+}
+
+#css-snippet {
+	display: none;
+}
+#css-snippet-type {
+	//display:none;
+}
+
+#typing-static {
+	white-space: pre;
+	font-family: 'Roboto Slab', serif;
+}
 </style>
