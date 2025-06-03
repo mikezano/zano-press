1a18942304a2380e3becc087d16a3786f3b60923
Zano <mikezano@gmail.com>
2025-06-02 19:55:58 -0700

    closer

--- .vitepress/components/showcase/AnimatedPathDrawing.vue
+++ .vitepress/components/showcase/AnimatedPathDrawing.vue
@@ -3,7 +3,7 @@
         <svg class=" drawn-path">
             <path :d="path" pathLength="1" />
         </svg>
-        <div class="pencil">✏️</div>
+        <div class="pencil" />
     </div>
 </template>
 
@@ -54,11 +54,13 @@ onMounted(() => {
     position: absolute;
     grid-column: 1;
     grid-row: 1;
-    height: var(--size);
-    width: var(--size);
 }
 
+
+
 .drawn-path {
+    height: var(--size);
+    width: var(--size);
     transform: translate(-50%, -50%);
 
     & path {
@@ -73,7 +75,11 @@ onMounted(() => {
 }
 
 .pencil {
-    transform: translateY(-20px) translateX(-4px);
+    position: absolute;
+    width: 7px;
+    height: 24px;
+    background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAYCAYAAADH2bwQAAAAAXNSR0IArs4c6QAAAdRJREFUOE9tkjtvE0EUhb8hfmQNHW4WEaREonRFg4AKiU0BLYVdIGio+SOho+EPRNBQh4bSQQIa0yVeOyKerJNgB+1slLWJL7qzxgTBSqt9nDPncWfMTCQ0G22LgGQTTDaBbMq30RE33jytGxEJZaNtDXhAXEEajI9Y2VTCTEJebltUQle7KWQ5++NjVjaf1c3CAlNYnCopZzA65rpXEAnjOLZBLcCljjRNcc5hraXVbBYWca9rg9plMiU4h3Mpg/0BzVbrgkJQ80ChkGLtAc2WKmiLYWRNuQr5EMktZpLQ311m9WE2b3H4wEqpiskTyA8gT+jHQUHwLQ4jy9IyTBQs7n43YPWRKsgslOG6NaWKByRPCotuwNofi8iiGSYJnFmfpd/VDKdFSIaRxSskhc1ZQi8OWPvLYt6CXBXU4mKLbmj5meh2La7vY6jfYd5i95onCIIRwMDoBK4qYdGiXPHe/8zBb7fOoVSFadFC8iF7iwzawg+q7AHjhzWv+b8Wv/eit6M1Xd1ooJ23d6Pzc7b0HREwhqVLsn7zcfu9P4oKdN692DLGRPp58mPy4d6TV/f1f0EAGo3GLRE+6dm8Uqne3v7y+aOCC8Kc9FyfXzud1+Lt4BfcN1azblSuCgAAAABJRU5ErkJggg==");
+    background-repeat: no-repeat;
     offset-path: var(--path);
     offset-rotate: 0deg;
     offset-distance: 0;
